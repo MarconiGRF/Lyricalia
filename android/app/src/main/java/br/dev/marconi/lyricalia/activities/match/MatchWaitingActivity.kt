bc7b902bb22707ccb6244d4ab342f2c0a6431c1f
Marconi Filho <18905896+MarconiGRF@users.noreply.github.com>
2025-06-27 08:23:14 -0300

    Adds animation to Match Waiting Room

--- android/app/src/main/java/br/dev/marconi/lyricalia/activities/match/MatchWaitingActivity.kt
+++ android/app/src/main/java/br/dev/marconi/lyricalia/activities/match/MatchWaitingActivity.kt
@@ -1,12 +1,13 @@
 package br.dev.marconi.lyricalia.activities.match
 
+import android.animation.LayoutTransition
+import android.content.res.Resources
 import android.graphics.Color
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -19,6 +20,10 @@ import br.dev.marconi.lyricalia.utils.NavigationUtils
 
 class MatchWaitingActivity: AppCompatActivity() {
     private lateinit var binding: ActivityMatchWaitingBinding
+    private lateinit var otherPlayersLayout: LinearLayout
+    private lateinit var playerColors: Array<Pair<Int, Int>>
+    private var currentColor = 0
+    private var players = mutableMapOf<String, Int>()
 
     override fun onStart() {
         super.onStart()
@@ -30,12 +35,25 @@ class MatchWaitingActivity: AppCompatActivity() {
         binding = ActivityMatchWaitingBinding.inflate(layoutInflater)
         setupMatchWaitingActivity()
 
-        binding.createMatchButton.setOnClickListener {
-            addPlayerOnView()
-        }
-        binding.deleteppl.setOnClickListener {
-            removePlayer(players.values.random())
-        }
+        playerColors = arrayOf(
+            Pair(resources.getColor(R.color.lyWhite, theme), resources.getColor(R.color.lyIndigo, theme)),
+            Pair(resources.getColor(R.color.lyWhite, theme), resources.getColor(R.color.lyDarkerGray, theme)),
+            Pair(resources.getColor(R.color.lyDarkerGray, theme), resources.getColor(R.color.lyGreen, theme)),
+            Pair(resources.getColor(R.color.lyGreen, theme), resources.getColor(R.color.lyGray, theme)),
+            Pair(resources.getColor(R.color.lyIndigo, theme), resources.getColor(R.color.lyDarkerGray, theme))
+        )
+
+        otherPlayersLayout = binding.otherPlayers
+        binding.createMatchButton.setOnClickListener { addPlayerOnView() }
+        binding.deleteppl.setOnClickListener { removePlayer(players.values.random()) }
+        val layoutTransition = LayoutTransition()
+        layoutTransition.enableTransitionType(LayoutTransition.APPEARING)
+        layoutTransition.enableTransitionType(LayoutTransition.CHANGE_APPEARING)
+        layoutTransition.enableTransitionType(LayoutTransition.DISAPPEARING)
+        layoutTransition.enableTransitionType(LayoutTransition.CHANGE_DISAPPEARING)
+        layoutTransition.setDuration(LayoutTransition.APPEARING, 350)
+        layoutTransition.setDuration(LayoutTransition.CHANGE_APPEARING, 350)
+        otherPlayersLayout.layoutTransition = layoutTransition
 
         val matchId = intent.extras!!.getString(NavigationUtils.MATCH_ID_PARAMETER_ID)!!
         binding.matchId.text = matchId
@@ -43,31 +61,38 @@ class MatchWaitingActivity: AppCompatActivity() {
         val isHost = intent.extras!!.getBoolean(NavigationUtils.IS_HOST_PARAMETER_ID)
     }
 
-    private var players = mutableMapOf<String, Int>()
     private fun addPlayerOnView() {
-        val playersContainer = binding.otherPlayers
-
         val playerIndicatorInstance = LayoutInflater.from(this)
-            .inflate(R.layout.player_indicator, playersContainer, false)
+            .inflate(R.layout.player_indicator, otherPlayersLayout, false)
 
-        val blueAmt = IntArray(256) { it + 1 }
-        playerIndicatorInstance.id = View.generateViewId()
-
-        if (players.size % 2 != 0) {
-            playerIndicatorInstance.translationY = 120f
-        }
         val playerName = ('A'..'Z').random().toString()
+        playerIndicatorInstance.id = View.generateViewId()
         players.put(playerName, playerIndicatorInstance.id)
 
         playerIndicatorInstance.findViewById<ImageView>(R.id.playerGlyphContainer)
-            .setColorFilter(Color.argb(255, 255, 255, blueAmt.random()))
-
+            .setColorFilter(playerColors[currentColor].second)
         playerIndicatorInstance.findViewById<TextView>(R.id.playerGlyph).also {
-            it.setTextColor(Color.argb(255, blueAmt.random(), 255, 255))
+            it.setTextColor(playerColors[currentColor].first)
             it.text = playerName
         }
+        currentColor = (currentColor + 1) % playerColors.size
 
-        playersContainer.addView(playerIndicatorInstance)
+        playerIndicatorInstance.translationY = 2000f
+        otherPlayersLayout.addView(playerIndicatorInstance)
+
+        if (players.size % 2 != 0) {
+            playerIndicatorInstance.animate()
+                .translationY(120f)
+                .setDuration(550)
+                .setInterpolator(android.view.animation.DecelerateInterpolator())
+                .start()
+        } else {
+            playerIndicatorInstance.animate()
+                .translationY(0f)
+                .setDuration(550)
+                .setInterpolator(android.view.animation.DecelerateInterpolator())
+                .start()
+        }
     }
     private fun removePlayer(playerIndicatorId: Int) {
         val playersContainer = binding.otherPlayers
